4bb9cb7019470846af0b86bbce75d61a1c436e71
Costa <andrecosta.cefet@gmail.com>
2018-02-23 20:09:56 -0400

    bug fix on HTTP Parser

--- parsers/http.js
+++ parsers/http.js
@@ -50,7 +50,7 @@ class HTTP_Parser
       if(tracker.get('lastCoordinate'))
       {
          //Request coordinates only after this moment
-         request += "?startDate=" + moment(tracker.get('lastCoordinate').datetime).add(1, 'minute').format('YYYY-MM-DDTHH:mm:ss-SSSS');//2018-02-03T00:00:00-0000"
+         request += "?startDate=" + moment(tracker.get('lastCoordinate').datetime).utc().add(1, 'second').format('YYYY-MM-DDTHH:mm:ss-0000');//2018-02-03T00:00:00-0000"
       }
 
       //Check if tracker is not currently being updated
@@ -78,53 +78,68 @@ class HTTP_Parser
                //Parse resulting buffer
                parser.parseString(buffer.toString(), function(err, result) 
                {
-                  if(err)
-                  {
-                     //Log error
-                     logger.error("Error parsing XML response from tracker " + tracker.name + ": " + err);
-
-                     //Inform error to tracker
-                     tracker.configError();
-                  }
-                  else 
-                  {
-                     try 
-                     {
-                        //Get how many results returned from query
-                        var total_results = result.response.feedMessageResponse[0].messages[0].message.length;
-
-                        //Check if any result available
-                        if(total_results > 0)
+                    try 
+                    {
+                        //Error parsing XML
+                        if(err)
                         {
-                           //Finished parsing data
-                           logger.info("Data available from tracker SPOT@" + tracker.get('name') + " XML feed");
+                            //Throw error
+                            throw err;
+                        }
+                        else 
+                        {
+                            //Check if result is an error message
+                            if(result.response.errors)
+                            {  
+                                //Throw error description
+                                throw result.response.errors[0].error[0].description[0];
+                            }
+                            else if(!result.response.feedMessageResponse)
+                            {
+                                //XML can't be parsed, throw error
+                                throw "Unknown XML structure";
+                            }
+                            else
+                            {
+                                //Get how many results returned from query
+                                var total_results = parseInt(result.response.feedMessageResponse[0].totalCount[0]);
 
-                           //For each result in feed
-                           result.response.feedMessageResponse[0].messages[0].message.reverse().forEach(function(message,index) 
-                           {
-                              //Parse data from message
-                              tracker.parseData(message, index, result.response.feedMessageResponse[0].messages[0].message.length);
-                           });
+                                //Finished parsing data
+                                logger.info("Data available from tracker SPOT@" + tracker.get('name') + " XML feed");
 
-                           //Remove flag after estimated process time
-                           setTimeout(() => { tracker.updateInProgress = false;}, total_results*1500);
+                                //For each result in feed
+                                result.response.feedMessageResponse[0].messages[0].message.reverse().forEach(function(message,index) 
+                                {
+                                    //Parse data from message
+                                    tracker.parseData(message, index, result.response.feedMessageResponse[0].messages[0].message.length);
+                                });
+
+                                //Remove flag after estimated process time
+                                setTimeout(() => { tracker.updateInProgress = false;}, total_results*1500);
+                            }
+                        } 
+                    }
+                    catch (error) 
+                    {
+                        //Check if it is an empty result request
+                        if(error.includes('No displayable messages'))
+                        {
+                            //Log data
+                            logger.debug('No updates on tracker SPOT@' + tracker.get('name'));
+
+                            //Flag update finished
+                            tracker.updateInProgress = false;
                         }
                         else
                         {
-                           //Flag indicating update operation finished
-                           tracker.updateInProgress = false;
-                        }
-                     } 
-                     catch (error) 
-                     {
-                        //Log error
-                        logger.error("Unexpected response in XML feed from " + tracker.name + ": " + error, result);
+                            //Log error
+                            logger.error("Unexpected response in XML feed from " + tracker.name + ": " + error, result);
 
-                        //Inform error to tracker
-                        tracker.configError();
-                     }
-                  }
-               });
+                            //Inform error to tracker
+                            tracker.configError();
+                        }
+                    }
+                });
             }));
          });
       }
